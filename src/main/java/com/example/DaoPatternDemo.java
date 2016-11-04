9df7cbb16cf7248c5c5324b374053ba3321d8871
stefanvlahov <stefan.vlahov5@gmail.com>
2016-11-03 22:58:48 -0500

    Initial Commit - DAO Pattern project

---
+++ src/main/java/com/example/DaoPatternDemo.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoPatternDemo {
+
+	public static void main(String[] args) {
+		StudentDao studentDao = new StudentDaoImpl();
+
+		for (Student student : studentDao.getAllStudents()) {
+			System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");
+		}
+
+		Student student = studentDao.getAllStudents().get(0);
+		student.setName("Michael");
+		studentDao.updateStudent(student);
+
+		studentDao.getStudent(0);
+        System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");
+
+		SpringApplication.run(DaoPatternDemo.class, args);
+
+	}
+}
